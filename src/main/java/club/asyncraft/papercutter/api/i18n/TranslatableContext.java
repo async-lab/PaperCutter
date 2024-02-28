e6f0eff23a928ee5ae5d2805f38dfeaa27e27658
dsx137 <1419696375@qq.com>
2024-02-29 07:49:01 +0800

    注释

--- src/main/java/club/asyncraft/papercutter/api/i18n/TranslatableContext.java
+++ src/main/java/club/asyncraft/papercutter/api/i18n/TranslatableContext.java
@@ -7,10 +7,23 @@ import java.io.File;
 import java.nio.file.Paths;
 import java.util.List;
 
+/**
+ * <p>国际化上下文</p>
+ * <p>语言文件夹为resources/locals</p>
+ * <p>实例化后使用实例的translate方法，输入完全限定名(类似于command.help)作为参数来获得字符串</p>
+ */
 public class TranslatableContext {
 
     public YamlMap langMap = new YamlMap();
 
+    /**
+     * <p>构造函数</p>
+     *
+     * @param plugin     插件实例
+     * @param langs      支持的语言列表
+     * @param langConfig 当前使用的语言
+     * @throws Exception 当语言文件不存在时抛出异常
+     */
     public TranslatableContext(JavaPlugin plugin, List<String> langs, String langConfig) throws Exception {
         //导出所有语言文件
         for (String lang : langs) {
